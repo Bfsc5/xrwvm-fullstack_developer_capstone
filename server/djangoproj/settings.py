db4d2bc30034cadeaa19c400c5a456d691932649
Bfsc5 <stevencruz5@hotmail.com>
2026-07-12 21:22:17 +0100

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -30,8 +30,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
-    'https://stevencruz5-8000.theiadockernext-1-labs-'
-    'prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+    (
+        'https://stevencruz5-8000.theiadockernext-1-labs-'
+        'prod-theiak8s-4-tor01.'
+        'proxy.cognitiveclass.ai'
+    ),
 ]
 CSRF_TRUSTED_ORIGINS = [
     'https://stevencruz5-8000.theiadockernext-1-'
